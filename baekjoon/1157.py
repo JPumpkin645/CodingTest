50af906e68c69613640bc13d919089d916817fdf
JPumpkin1223 <jpark9595@naver.com>
2024-07-29 01:58:51 +0900

    Feat: Add functionality to determine the most frequent character in a string - Implement logic to find the character(s) with the highest frequency in the input string. Output '?' if there are ties.

---
+++ baekjoon/1157.py
@@ -0,0 +1,17 @@
+import sys
+from collections import defaultdict
+
+read = sys.stdin.readline().rstrip().upper()
+
+dict = defaultdict(int)
+
+for item in read:
+    dict[item] += 1
+
+max_value = max(dict.values())
+answer = [key for key, value in dict.items() if value == max_value]
+
+if len(answer) > 1:
+    print("?")
+else:
+    print(answer[0])
