2dc025a5e432a51523a85407a6fc09b19a7796d2
JPumpkin1223 <jpark9595@naver.com>
2024-08-04 01:04:53 +0900

    Feat: Add optimized scoring logic for more efficient calculations
    - Modify scoring list initialization to streamline array creation.
    - Implement conditional assignment for specific score calculations based on input size.
    - Improve scoring calculation efficiency for better performance.

--- baekjoon/2579.py
+++ baekjoon/2579.py
@@ -1,13 +1,14 @@
 import sys
 
 n = int(input())
-score_list = [0] * 301
-for i in range(1, n + 1):
-    score_list[i] = int(sys.stdin.readline().rstrip())
+score_list = [0] + [int(sys.stdin.readline().rstrip()) for _ in range(n)]
 ac_list = [0] * 301
-ac_list[1] = score_list[1]
-ac_list[2] = score_list[1] + score_list[2]
-ac_list[3] = max(score_list[1] + score_list[3], score_list[2] + score_list[3])
+if n >= 1:
+    ac_list[1] = score_list[1]
+if n >= 2:
+    ac_list[2] = score_list[1] + score_list[2]
+if n >= 3:
+    ac_list[3] = max(score_list[1] + score_list[3], score_list[2] + score_list[3])
 
 for i in range(4, n + 1):
     ac_list[i] = max(ac_list[i - 3] + score_list[i - 1] + score_list[i], ac_list[i - 2] + score_list[i])
