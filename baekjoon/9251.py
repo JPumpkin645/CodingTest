64644c8dd98cf55211fb4922c57ce86ddc1316b2
JPumpkin1223 <jpark9595@naver.com>
2024-08-09 16:20:11 +0900

    Feat: Implement algorithm to find longest common subsequence length - Added code to find the length of longest common subsequence between two strings.

---
+++ baekjoon/9251.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+a = input().rstrip()
+b = input().rstrip()
+
+dp = [[0] * (len(a) + 1) for _ in range(len(b) + 1)]
+
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i - 1] == b[j - 1]:
+            dp[j][i] = dp[j - 1][i - 1] + 1
+        else:
+            dp[j][i] = max(dp[j][i - 1], dp[j - 1][i])
+
+print(dp[len(b)][len(a)])
